e8bf02b6eae19d3747d460376324ffd64cfc3938
Michael Ottey <mottey@gmail.com>
2016-08-02 11:11:16 +1000

    lazyload currentSlide fix.

--- src/inner-slider.jsx
+++ src/inner-slider.jsx
@@ -14,6 +14,7 @@ import {PrevArrow, NextArrow} from './arrows';
 export var InnerSlider = React.createClass({
   mixins: [HelpersMixin, EventHandlersMixin],
   getInitialState: function () {
+    initialState.currentSlide = this.props.initialSlide;
     return initialState;
   },
   getDefaultProps: function () {
